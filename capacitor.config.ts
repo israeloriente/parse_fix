8a12d346335aca92fd2659ca364c2376ffd9d4a0
israeloriente <israelnunesoriente@gmail.com>
2022-11-20 13:04:05 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'parse_problem',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
